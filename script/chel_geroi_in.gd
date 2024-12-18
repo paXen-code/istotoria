e025ae3665246b8fa61a117d21d220d9a9dac2e8
paXen-code <pavel.a.krut@gmail.com>
2024-12-18 20:48:34 +0300

    123123123123123123123123123123

--- script/chel_geroi_in.gd
+++ script/chel_geroi_in.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
-var zona = false
+var zona_izby = false
+var zona_time = false
+var pause = false
 
 func _physics_process(_delta):
 	if Input.is_action_pressed('ui_left'):
@@ -13,16 +15,42 @@ func _physics_process(_delta):
 	if not Input.is_action_pressed('ui_down') and not Input.is_action_pressed('ui_up') and not Input.is_action_pressed('ui_right') and not Input.is_action_pressed('ui_left'):
 		velocity = Vector2(0, 0)
 	move_and_slide()
+	
+	if Input.is_action_just_pressed('ui_cancel') and pause == false:
+		get_tree().paused = true
+		pause = true
+		$pausa.show()
+	elif Input.is_action_just_pressed('ui_cancel') and pause:
+		get_tree().paused = false
+		pause = false
+		$pausa.hide()
 
 func _input(event):
 	if event is InputEventKey:
 		if event.keycode == 69:
-			if zona:
-				zona = false
-				get_tree().change_scene_to_file("res://scene/izba_1.tscn")
+			if zona_izby:
+				zona_izby = false
+				get_parent().get_parent().get_tree().change_scene_to_file("res://scene/izba_1.tscn")
+			elif zona_time:
+				zona_time = false
+				get_parent().get_parent().get_tree().change_scene_to_file("res://scene/play.tscn")
 
 func _on_area_2d_body_entered(_body):
-	zona = true
+	zona_izby = true
 
 func _on_area_2d_body_exited(_body):
-	zona = false
+	zona_izby = false
+
+func _on_exit_pressed():
+	get_parent().get_tree().quit()
+
+func _on_return_pressed():
+	get_parent().get_tree().paused = false
+	pause = false
+	$pausa.hide()
+
+func _on_time_machine_1_body_entered(_body):
+	zona_time = true
+
+func _on_time_machine_1_body_exited(_body):
+	zona_time = false
